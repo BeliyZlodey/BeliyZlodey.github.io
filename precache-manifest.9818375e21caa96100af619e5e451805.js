56760b0cb688a501f1d96954d4b7bbd80f70cca1
Матвей Лифшиц <m.lifshits@sitesoft.ru>
2022-01-16 17:39:23 +0500

    init

---
+++ precache-manifest.9818375e21caa96100af619e5e451805.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacb9a1e1bc4898db948",
+    "url": "css/app.8a70da76.css"
+  },
+  {
+    "revision": "62ac9d6b75964b7a021a",
+    "url": "css/chunk-vendors.5d71e361.css"
+  },
+  {
+    "revision": "04fdcb923ddc548c325857d0785d869b",
+    "url": "img/logo.04fdcb92.png"
+  },
+  {
+    "revision": "6861ec4fe3119b37c64ff20d9e123d18",
+    "url": "index.html"
+  },
+  {
+    "revision": "cacb9a1e1bc4898db948",
+    "url": "js/app.87912d57.js"
+  },
+  {
+    "revision": "62ac9d6b75964b7a021a",
+    "url": "js/chunk-vendors.073cf881.js"
+  },
+  {
+    "revision": "989e20507165b136e60c95e67c650cc7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
